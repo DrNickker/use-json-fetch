cee9fe833a323c82232259a58e094094b285892c
DrNickker <mintagirov2015@yandex.ru>
2023-07-13 14:54:09 +0500

    1

--- backend/server.js
+++ backend/server.js
@@ -34,5 +34,4 @@ app.get("/loading", async (req, res) => {
 
 const port = process.env.PORT || 7070;
 app.listen(port, () => console.log(`The server is running on port ${port}.`));
-// eslint-disable-next-line no-unused-expressions
 ``;
